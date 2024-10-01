89e4b59d4dc35b2ad3984425aec89f2e268457a6
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-10-01 20:25:35 +0100

    C2

---
+++ bank-account-app/angular-front-end/src/app/accounts/accounts.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, NgFor, NgIf} from "@angular/common";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@Component({
+  selector: 'app-accounts',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgFor,
+    CommonModule,
+    HttpClientModule
+  ],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent implements OnInit{
+  accounts : any
+   constructor(private http : HttpClient) {
+  }
+ngOnInit() {
+    this.http.get("http://localhost:8888/ACCOUNT-SERVICE/accounts")
+      .subscribe({
+        next : data => {
+          this.accounts = data;
+        },
+        error : err => {
+          console.log(err)
+        }
+      })
+}
+}
